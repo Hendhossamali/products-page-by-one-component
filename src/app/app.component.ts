94fb245c9c44c960374069d3057808defd484f21
hend <hendhossam36@gmail.com>
2023-12-07 21:06:48 +0200

    creating-products-store

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, ProductsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'prodcts-one-page';
+}
